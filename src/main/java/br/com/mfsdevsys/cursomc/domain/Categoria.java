b6740f9ebdd343463ed04394f1a616b322c98b1f
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-12-20 16:08:02 -0400

    Banco de dados postgreesql e criação da base de dados

--- src/main/java/br/com/mfsdevsys/cursomc/domain/Categoria.java
+++ src/main/java/br/com/mfsdevsys/cursomc/domain/Categoria.java
@@ -3,11 +3,24 @@ package br.com.mfsdevsys.cursomc.domain;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(nullable=false, length =60)
 	private String nome;
 	
 	public Categoria() {
--- src/main/java/br/com/mfsdevsys/cursomc/domain/Produto.java
+++ src/main/java/br/com/mfsdevsys/cursomc/domain/Produto.java
@@ -9,14 +9,16 @@ public class Produto implements Serializable {
 	
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
 	public Produto() {
 		
 	}
 
-	public Produto(Integer id, String nome) {
+	public Produto(Integer id, String nome,Double preco) {
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -33,6 +35,16 @@ public class Produto implements Serializable {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	
+	
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
 	@Override
