84f40ae0a9b835a7db43822b55882c845ed0bb99
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2022-12-22 10:38:41 -0400

    Pagination

--- src/main/java/br/com/mfsdevsys/cursomc/services/CategoryService.java
+++ src/main/java/br/com/mfsdevsys/cursomc/services/CategoryService.java
@@ -1,14 +1,14 @@
 package br.com.mfsdevsys.cursomc.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,10 +34,10 @@ public class CategoryService {
 	}
 	
 	@Transactional(readOnly=true)
-	public List<CategoryDTO> findAll(){
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest){
 		
-		List<Category> list = repository.findAll();
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		Page<Category> list = repository.findAll(pageRequest);
+		return list.map(x -> new CategoryDTO(x));
 		
 	}
 
